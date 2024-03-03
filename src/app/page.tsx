9eb742b480267e8490fe2c3e8d366a5f82e752ec
HieuTrung15 <hieutrungvietnam0107@gmail.com>
2024-03-03 20:10:36 +0700

    add highchart error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import TopMenu from '../components/TopMenu/TopMenu';
+import Chart from '@/components/HighStockChart/HighStockChart';
 
 const Home = () => {
+  const options: Highcharts.Options = {
+    chart: {
+      type: 'bar',
+    },
+    title: {
+      text: 'Biểu đồ mẫu',
+    },
+    series: [
+      {
+        name: 'Dữ liệu mẫu',
+        type: 'bar',
+        data: [1, 2, 3, 4, 5],
+      },
+    ],
+  };
   return (
     <div>
       <TopMenu />
+      <Chart options={options} />
     </div>
   );
 };
---
+++ src/components/HighStockChart/HighStockChart.tsx
@@ -0,0 +1,14 @@
+// components/Chart.tsx
+// import React from 'react';
+// import Highcharts from 'highcharts';
+// import HighchartsReact from 'highcharts-react-official';
+
+// interface ChartProps {
+//   options: Highcharts.Options;
+// }
+
+// const Chart: React.FC<ChartProps> = () => {
+//   return ;
+// };
+
+// export default Chart;
